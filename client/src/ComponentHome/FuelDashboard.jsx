04f5c5eb92b09a8a6b90ebfb4948716e645acaa5
senura upesh <it22179180@my.sliit.lk>
2025-11-16 12:56:11 +0530

    Removed the icon of the "Sign Up" from this page

--- client/src/ComponentHome/FuelDashboard.jsx
+++ client/src/ComponentHome/FuelDashboard.jsx
@@ -6,16 +6,13 @@ import {
   ListItemIcon,
   ListItemText,
   Divider,
-  Box,
-  IconButton,
-  Tooltip
+  Box
 } from "@mui/material";
 import HomeIcon from '@mui/icons-material/Home';
 import LocalGasStationIcon from '@mui/icons-material/LocalGasStation';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import InfoIcon from '@mui/icons-material/Info';
 import SettingsIcon from '@mui/icons-material/Settings';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import { useNavigate } from "react-router-dom";
 
 function FuelDashboard() {
@@ -30,10 +27,6 @@ function FuelDashboard() {
     { label: "Notifications", icon: <NotificationsActiveIcon />, path: "/notifications" },
     { label: "Settings", icon: <SettingsIcon />, path: "/settings" }
   ];
-
-  const handleRegisterClick = () => {
-    navigate("/register");
-  };
 
   return (
     <Box sx={{ display: "flex", position: "relative" }}>
@@ -73,33 +66,6 @@ function FuelDashboard() {
         </List>
         <Divider sx={{ bgcolor: "#fff" }} />
       </Drawer>
-
-      {/* Register button positioned top right */}
-      <Box
-        sx={{
-          position: "fixed",
-          top: 16,
-          right: 16,
-          zIndex: 1300, // higher than Drawer
-        }}
-      >
-        <Tooltip title="Register">
-          <IconButton
-            color="primary"
-            onClick={handleRegisterClick}
-            sx={{
-              bgcolor: "#351B65",
-              color: "#fff",
-              "&:hover": {
-                bgcolor: "#9667D9"
-              }
-            }}
-            size="large"
-          >
-            <PersonAddIcon />
-          </IconButton>
-        </Tooltip>
-      </Box>
     </Box>
   );
 }
