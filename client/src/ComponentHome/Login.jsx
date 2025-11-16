ce9b1eb16efc0af1183ba7a42380391502f4fa1c
senura upesh <it22179180@my.sliit.lk>
2025-11-16 14:58:19 +0530

    Added to navigation path after a successful login

--- client/src/ComponentHome/Login.jsx
+++ client/src/ComponentHome/Login.jsx
@@ -22,6 +22,7 @@ function Login() {
       const result = await response.json();
       if (response.ok) {
         alert("Login successful");
+        navigate("/FuelDashboard");
         // Redirect or store user info/token as needed
       } else {
         setError(result.error || "Login failed");
