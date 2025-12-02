c0c1165e67d8d17d65166450ff065547b918c081
Thiwurani Dilmani Aluthge <154864435+thiwuraniAluthge@users.noreply.github.com>
2025-12-02 12:54:31 +0530

    feat: Create basic ReportCharts component
    Adds a placeholder ReportCharts.jsx component for admin reports and analytics.

---
+++ frontend/src/modules/admin/components/ReportCharts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReportCharts = () => {
+  return (
+    <div>
+      <h3>Report Charts Component</h3>
+      {/* Report charts content will go here */}
+    </div>
+  );
+};
+
+export default ReportCharts;
