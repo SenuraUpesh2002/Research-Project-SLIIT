5ad32dad89b4d2f83f22723f82301a32c54e3638
Thiwurani Dilmani Aluthge <154864435+thiwuraniAluthge@users.noreply.github.com>
2025-12-02 12:54:13 +0530

    feat: Create basic UserTable component
    Adds a placeholder UserTable.jsx component for admin user management.

---
+++ frontend/src/modules/admin/components/UserTable.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserTable = () => {
+  return (
+    <div>
+      <h3>User Table Component</h3>
+      {/* User table content will go here */}
+    </div>
+  );
+};
+
+export default UserTable;
