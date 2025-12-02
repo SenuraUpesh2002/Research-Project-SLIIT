ec5bc009d280b547205dee955b0f9b311cefb252
Thiwurani Dilmani Aluthge <154864435+thiwuraniAluthge@users.noreply.github.com>
2025-12-02 12:54:38 +0530

    feat: Create basic SubmissionTable component
    Adds a placeholder SubmissionTable.jsx component for admin user submissions management.

---
+++ frontend/src/modules/admin/components/SubmissionTable.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SubmissionTable = () => {
+  return (
+    <div>
+      <h3>Submission Table Component</h3>
+      {/* Submission table content will go here */}
+    </div>
+  );
+};
+
+export default SubmissionTable;
