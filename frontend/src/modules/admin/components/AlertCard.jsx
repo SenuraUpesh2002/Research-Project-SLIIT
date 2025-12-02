bcc6bb07a6c3a70969cff9ebf2b0bb6579c72fb9
Thiwurani Dilmani Aluthge <154864435+thiwuraniAluthge@users.noreply.github.com>
2025-12-02 12:54:23 +0530

    feat: Create basic AlertCard component
    Adds a placeholder AlertCard.jsx component for admin alert/notification management.

---
+++ frontend/src/modules/admin/components/AlertCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AlertCard = () => {
+  return (
+    <div>
+      <h3>Alert Card Component</h3>
+      {/* Alert card content will go here */}
+    </div>
+  );
+};
+
+export default AlertCard;
