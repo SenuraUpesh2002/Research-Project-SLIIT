5fdc59f92f579aba59657dbf5ebe72ea8cf78c96
Thiwurani Dilmani Aluthge <154864435+thiwuraniAluthge@users.noreply.github.com>
2025-12-03 06:50:24 +0530

    feat: Add global.css, variables.css, App.jsx, and index.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './router';
+import { AuthProvider } from './hooks/useAuth';
+import { UiProvider } from './hooks/useUiStore'; // Assuming you'll create this
+
+function App() {
+  return (
+    <AuthProvider>
+      {/* <UiProvider> */}
+        <Router>
+          <AppRoutes />
+        </Router>
+      {/* </UiProvider> */}
+    </AuthProvider>
+  );
+}
+
+export default App;
